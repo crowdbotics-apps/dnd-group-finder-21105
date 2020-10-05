89db657561ad793676fdc1257e625181e5de975e
Crowdbotics <team@crowdbotics.com>
2020-10-05 13:34:22 +0000

    Initial commit.

---
+++ backend/dnd_group_finder_21105/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dnd_group_finder_21105 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dnd_group_finder_21105.settings')
+
+application = get_wsgi_application()
